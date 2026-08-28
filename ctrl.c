fae0d2c4c828c627aa169c398aad014a63d67a40
Siddhant Hullur <hullur@wisc.edu>
2026-08-28 19:29:58 +0300

    wrote basic spam A button code

--- ctrl.c
+++ ctrl.c
@@ -1,14 +1,65 @@
-#include <stdio.h>
-#include "pico/stdlib.h"
+#include "bsp/board_api.h"
+#include "tusb.h"
+#include <stdint.h>
 
+void hid_task(void);
 
+struct controllerPacket {
+    uint8_t reportID;
+    uint8_t sequenceID;
+    uint8_t connectionBattery;
+    uint8_t rightButtons;
+    uint8_t middleButtons;
+    uint8_t leftButtons;
+    uint8_t leftStick[3];
+    uint8_t rightStick[3];
+    int  padding[13];
+};
 
-int main()
+int main(void)
 {
-    stdio_init_all();
+    board_init();
+    tusb_init();
 
-    while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+    while(1) {
+        tud_task(); // tinyUSB method for processing USB events
+        hid_task(); // user defined
     }
+}
+
+void A_buttonSpam(struct controllerPacket* packet, uint8_t counter) {
+    packet->reportID = 0x30;
+    packet->sequenceID = counter;
+    packet->connectionBattery = 0x91; //replace later
+    packet->rightButtons = 0x08; // A button spam
+    packet->middleButtons = 0x00;
+    packet->leftButtons = 0x00;
+
+    packet->leftStick[0] = 0x00;
+    packet->leftStick[1] = 0x08;
+    packet->leftStick[2] = 0x80;
+
+    packet->rightStick[0] = 0x00;
+    packet->rightStick[1] = 0x08;
+    packet->rightStick[2] = 0x80;
+}
+
+void hid_task(void) {
+
+    const uint32_t interval_ms = 10;
+    static uint32_t start_ms = 0;
+
+    if (board_millis() - start_ms < interval_ms) return;
+    start_ms += interval_ms;
+
+    if (!tud_hid_ready()) return; 
+
+    static uint8_t counter = 0;
+
+    struct controllerPacket packet;
+    memset(&packet, 0, sizeof(packet)); 
+    A_buttonSpam(&packet, counter);
+    counter += 1;
+
+    tud_hid_report(0, &packet, 64);
 }
--- tusb_config.h
+++ tusb_config.h
@@ -6,12 +6,17 @@
 #endif
 
 // Raspberry Pi Pico 2 Setup
+#ifndef CFG_TUSB_MCU
 #define CFG_TUSB_MCU                OPT_MCU_RP2350
+#endif
+
+#ifndef CFG_TUSB_MCU
 #define CFG_TUSB_OS                 OPT_OS_NONE
+#endif
 
 #define CFG_TUSB_DEBUG              0 // no debug output
 #define CFG_TUD_ENABLED             1 // rpi should operate in device mode
-#define CFG_TUD_ENDPOINT0_SIZE      64 // 64 bit max size on default channel
+#define CFG_TUD_ENDPOINT0_SIZE      64 // 64 byte max size on default channel
 
 // actual usb port should run as a device, at full speed (best latency)
 // - device -> this microcontroller is plugging into another computer
@@ -20,7 +25,8 @@
 
 // configuring the USB class this should act as - HID
 #define CFG_TUD_HID                 1
-#define CFG_TUD_HID_EP_BUFSIZE      64 // buffers sent are 64 bit in size
+#define CFG_TUD_HID_BUFSIZE         64
+#define CFG_TUD_HID_EP_BUFSIZE      64 // buffers sent are 64 bytes in size
 
 // Disable all other standard classes - this is only an HID
 #define CFG_TUD_CDC                 0
